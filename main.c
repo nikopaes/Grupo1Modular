42cc74f4329bf9386d45593cbc147f831f8d6461
Marcell666 <brucemarcellino@hotmail.com>
2017-10-02 12:25:26 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "professor.h"
+
+#define MAX_PROFS 10
+
+Prof *prof[5] = {NULL, NULL, NULL, NULL, NULL};
+
+void fazSentido(void);
+
+int main(void){
+	//Prof *prof[MAX_PROFS];
+
+	int i;
+	PRF_tpCondRet var;
+	//for(i=0; i<MAX_PROFS; i++) prof[i]=NULL;
+	i=0;
+	printf("hello world\n");
+	var = PRF_cria( prof+i, "Bruce" , 111222333, "12345678901", 1112223, "bmMail@mail", 111222333, 11, 12, 2196, "BrasilComS", "RJ", "RioDeJaneiro", "umBairro", "AvQalquer", 666, "defrontePuc");
+	printf("var:%d\n", var);
+	PRF_mostra(prof[i]);
+	//alteraProf(prof, "felipe", "fse@gmail.com", 131312, 32342, "asdasdasas", 3123, 21, 4, 1924, 13123, 2123);
+	printf("vamos alterar cpf");
+	printf("%d\n", PRF_alteraCpf(prof[i], "10987654321"));
+	PRF_mostra(prof[i]);
+	PRF_libera(prof+i);
+	printf("%d\n", prof==NULL);
+	PRF_mostra(prof[i]);
+	printf("FIM\n");
+	return 0;
+}
